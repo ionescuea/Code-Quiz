3e6b7e79d67d06e2080cb028021964ed1f64ccb9
ionescuea <ionescuea@yahoo.com>
2024-01-15 23:47:57 +0000

    Add questions and logic

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,88 @@
-var startButton = document.querySelector(".start");
+var timerEl = document.querySelector("#time");
+var startScreen = document.querySelector("#start-screen");
+var startButton = document.querySelector("#start");
+var questions = document.querySelector("#questions");
+var questionTitle = document.querySelector("#question-title");
+var choices = document.querySelector("#choices");
+var answer = document.querySelector("#answer");
+var i = 0;
+var timer = 30;
+var endScreen = document.querySelector("#end-screen");
+var score = document.querySelector("#final-score");
+
+function addQuestion(q){
+    // console.log(questionTitle.textContent);
+    
+    questionTitle.textContent= q.question;
+    console.log(questionTitle.textContent);
+    choices.innerHTML = `
+        <button id="answer">${q.option1}</button>
+        <button>${q.option2}</button>
+        <button>${q.option3}</button>
+    `;
+}
+
+function time() {
+    var timeInterval = setInterval(function () {
+        // As long as the `timeLeft` is greater than 1
+        if (timer > 1) {
+          // Set the `textContent` of `timerEl` to show the remaining seconds
+            timerEl.textContent = timer;
+          // Decrement `timeLeft` by 1
+            timer--;
+        } else if (timer === 1) {
+          // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+            timerEl.textContent = timer;
+            timer--;
+        } else {
+          // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+          timerEl.textContent = 0;
+            endGame();
+        }
+        }, 1000);
+    }
+
+function endGame() {
+    questions.classList.add("hide");
+    endScreen.classList.remove("hide")
+}
+
+function nextQuestion() {
+    answer = document.querySelector("#answer");
+    console.log(answer.textContent);
+    // if answer.textContent != quiz[i].answer
+    if (answer.textContent != quiz[i].correctAnswer) {
+        // reduce time
+        timer -= 10;
+    }
+      // else if (answer.textContent = quiz[i].correctAnswer)
+      //   score = 0;
+      //   score++;
+    if (i < quiz.length-1) {
+        i++;
+        addQuestion(quiz[i]);
+    } else {
+        // else show end screen
+        endGame();
+    }
+
+    // on submit add results to local storage
+}
+
+
+function onButtonClick() {
+    startScreen.innerHTML = "";
+    questions.classList.remove("hide");
+    addQuestion(quiz[i]);
+    time();
+}
+
+startButton.addEventListener("click", onButtonClick)
+// NOTE! need to add timer when the button is clicked
+
+
+
+choices.addEventListener("click", nextQuestion)
+
+
+// on highscores, show highscore page
---
+++ assets/js/questions.js
@@ -0,0 +1,36 @@
+var quiz = [{
+    question: 'What is the capital city of Italy?',
+    option1: 'London',
+    option2: 'Paris',
+    option3: 'Rome',
+    correctAnswer: "Rome"
+},
+{
+    question: 'What is the capital city of Greece?',
+    option1: 'Paris',
+    option2: 'Athens',
+    option3: 'Dublin',
+    correctAnswer: "Athens"
+},
+{
+    question: 'What is the capital city of France?',
+    option1: 'Budapest',
+    option2: 'Paris',
+    option3: 'Ankara',
+    correctAnswer: "Paris"
+},
+{
+    question: 'What is the capital city of Portugal?',
+    option1: 'Dublin',
+    option2: 'Paris',
+    option3: 'Lisbon',
+    correctAnswer: "Lisbon"
+},
+{
+    question: 'What is the capital city of Egypt?',
+    option1: 'Athens',
+    option2: 'Cairo',
+    option3: 'Rome',
+    correctAnswer: "Cairo"
+}
+]
