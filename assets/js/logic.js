40b50657472c4a8959f764bfaaae22afab41db86
ionescuea <ionescuea@yahoo.com>
2024-01-16 16:14:15 +0000

    Add logic for questions flow

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,85 +4,73 @@ var startButton = document.querySelector("#start");
 var questions = document.querySelector("#questions");
 var questionTitle = document.querySelector("#question-title");
 var choices = document.querySelector("#choices");
-var answer = document.querySelector("#answer");
 var i = 0;
 var timer = 30;
+var score = 0;
 var endScreen = document.querySelector("#end-screen");
-var score = document.querySelector("#final-score");
+var finalScore = document.querySelector("#final-score");
+var clock = "";
 
-function addQuestion(q){
-    // console.log(questionTitle.textContent);
-    
-    questionTitle.textContent= q.question;
-    console.log(questionTitle.textContent);
-    choices.innerHTML = `
-        <button id="answer">${q.option1}</button>
-        <button>${q.option2}</button>
-        <button>${q.option3}</button>
-    `;
+function addQuestion(q) {
+  questionTitle.textContent = q.question;
+  choices.innerHTML = `
+      <button>${q.option1}</button>
+      <button>${q.option2}</button>
+      <button>${q.option3}</button>
+  `;
+  for (var j = 0; j<choices.children.length; j++) {
+    choices.children[j].addEventListener("click", nextQuestion);
+  }
 }
 
 function time() {
-    var timeInterval = setInterval(function () {
-        // As long as the `timeLeft` is greater than 1
-        if (timer > 1) {
-          // Set the `textContent` of `timerEl` to show the remaining seconds
-            timerEl.textContent = timer;
-          // Decrement `timeLeft` by 1
-            timer--;
-        } else if (timer === 1) {
-          // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-            timerEl.textContent = timer;
-            timer--;
-        } else {
-          // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-          timerEl.textContent = 0;
-            endGame();
-        }
-        }, 1000);
+  clock = setInterval(function () {
+    timerEl.textContent = timer;
+    if (timer >= 1) {
+      timer--;
+    } else {
+      timerEl.textContent = 0;
+      endGame();
     }
+  }, 1000);
+}
 
 function endGame() {
-    questions.classList.add("hide");
-    endScreen.classList.remove("hide")
+  clearInterval(clock);
+  questions.classList.add("hide");
+  endScreen.classList.remove("hide");
+  finalScore.textContent = score;
 }
 
 function nextQuestion() {
-    answer = document.querySelector("#answer");
-    console.log(answer.textContent);
-    // if answer.textContent != quiz[i].answer
-    if (answer.textContent != quiz[i].correctAnswer) {
-        // reduce time
-        timer -= 10;
-    }
-      // else if (answer.textContent = quiz[i].correctAnswer)
-      //   score = 0;
-      //   score++;
-    if (i < quiz.length-1) {
-        i++;
-        addQuestion(quiz[i]);
-    } else {
-        // else show end screen
-        endGame();
-    }
+  if (this.textContent != quiz[i].correctAnswer) {
+    // reduce time if wrong answer
+    timer -= 10;
+  }
+  else {
+    score++;
+  }
+  if (i < quiz.length - 1) {
+    i++;
+    addQuestion(quiz[i]);
+  } else {
+    // else show end screen
+    endGame();
+  }
 
-    // on submit add results to local storage
+  // on submit add results to local storage
 }
 
 
 function onButtonClick() {
-    startScreen.innerHTML = "";
-    questions.classList.remove("hide");
-    addQuestion(quiz[i]);
-    time();
+  startScreen.innerHTML = "";
+  questions.classList.remove("hide");
+  addQuestion(quiz[i]);
+  time();
 }
 
 startButton.addEventListener("click", onButtonClick)
-// NOTE! need to add timer when the button is clicked
 
-
-
-choices.addEventListener("click", nextQuestion)
 
 
 // on highscores, show highscore page
